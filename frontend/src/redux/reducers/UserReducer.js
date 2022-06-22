80e06c517f95dbc022f5ab12a22e886f11199242
Fortune Codebox <fortunecodebox@icloud.com>
2022-06-21 22:39:44 -0400

    load user

--- frontend/src/redux/reducers/UserReducer.js
+++ frontend/src/redux/reducers/UserReducer.js
@@ -7,6 +7,9 @@ import {
 	USER_REGISTER_FAIL,
 	USER_REGISTER_REQUEST,
 	USER_REGISTER_SUCCESS,
+	LOAD_USER_REQUEST,
+	LOAD_USER_SUCCESS,
+	LOAD_USER_FAIL
 } from "../constants/Constant";
 
 // REGISTER
@@ -14,26 +17,33 @@ export const userRegisterReducer = (state = {}, action) => {
 	switch (action.type) {
 		case USER_REGISTER_REQUEST:
 			return { loading: true };
+
 		case USER_REGISTER_SUCCESS:
 			return { loading: false, userInfo: action.payload };
+
 		case USER_REGISTER_FAIL:
 			return { loading: false, error: action.payload };
+
 		case CLEAR_ERRORS:
 			return {...state,error: null}
+
 		default:
 			return state;
 	}
 };
 
 // LOGIN
-export const userLoginReducer = (state = {}, action) => {
+export const userLoginReducer = (state = {userInfo = {}}, action) => {
 	switch (action.type) {
 		case USER_LOGIN_REQUEST:
-			return { loading: true ,isAuthenticated:false};
+		case LOAD_USER_REQUEST:
+			return { loading: true ,isAuthenticated:false, userInfo: {}};
 		case USER_LOGIN_SUCCESS:
-			return { loading: false, userInfo: action.payload,isAuthenticated:true };
+		case LOAD_USER_SUCCESS:
+			return { loading: false, userInfo: action.payload, isAuthenticated:true };
 		case USER_LOGIN_FAIL:
-			return { loading: false, error: action.payload, isAuthenticated:false };
+		case LOAD_USER_FAIL:
+			return { loading: false, error: action.payload, userInfo: null, isAuthenticated:false };
 		case USER_LOGOUT:
 			return {};
 		case CLEAR_ERRORS:
